29732b526e0122dc453c7b55b11d7a229906aa3e
Nasius <athanas.papn@gmail.com>
2020-01-10 03:38:15 +0200

    next kinda works

---
+++ js/buttons.js
@@ -0,0 +1,17 @@
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const pages = document.querySelectorAll('.page');
+
+var i = 0;
+var l = pages.length;
+
+next.addEventListener('click',function(e) {
+    pages[i].classList.remove('fg');
+    pages[i].classList.add('bg');
+    i=++i%l;
+    pages[i].classList.remove('bg');
+    pages[i].classList.add('fg');
+});
+prev.addEventListener('click',function(e) {
+    alert('Oops doesn\'t work');
+});
